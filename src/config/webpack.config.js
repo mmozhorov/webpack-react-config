bc0de3e9fefaf6763d96bed0bc8e32e11dae97d6
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2020-03-23 15:13:05 +0300

    Configure babel

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -2,12 +2,22 @@ const webpack = require('webpack');
 const path = require('path');
 const BUILD_DIRNAME = path.join(__dirname, '../../dist');
 const APP_DIRNAME = path.join(__dirname, '..');
+const JSAndJSXApplicationRule = {
+    test: /\.(js|jsx)/,
+    include: APP_DIRNAME,
+    use: 'babel-loader'
+};
 
 const config = {
     entry: APP_DIRNAME + '/index.js',
     output: {
         path: BUILD_DIRNAME,
         filename: './bundle.js'
+    },
+    module: {
+        rules: [
+            JSAndJSXApplicationRule
+        ]
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 console.log("Hello world");
 console.log("Hello world");
 console.log("Hello world");
+async function f (a) {
+    return await Promise.resolve(1 + a);
+}
+
+f(78).then(console.log);
