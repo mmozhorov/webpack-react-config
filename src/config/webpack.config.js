08904e69c3317ce85b34991b8359cd42ff13f7b9
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2020-03-23 17:07:26 +0300

    Configure css-loader and file-loader

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -2,10 +2,21 @@ const webpack = require('webpack');
 const path = require('path');
 const BUILD_DIRNAME = path.join(__dirname, '../../dist');
 const APP_DIRNAME = path.join(__dirname, '..');
-const JSAndJSXApplicationRule = {
+
+const babelLoader = {
     test: /\.(js|jsx)/,
     include: APP_DIRNAME,
     use: 'babel-loader'
+};
+
+const cssLoader = {
+    test: /\.css/,
+    use: [ 'style-loader', 'css-loader']
+};
+
+const imageLoader = {
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    use: 'file-loader'
 };
 
 const config = {
@@ -16,7 +27,9 @@ const config = {
     },
     module: {
         rules: [
-            JSAndJSXApplicationRule
+            babelLoader,
+            cssLoader,
+            imageLoader
         ]
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './main.css';
+import Logo from './logo.jpg';
 
 class App extends Component{
     render() {
         return(
-            <div>Hello world</div>
+            <>
+                <div>Hello world</div>
+                <img src={Logo}  alt="logo"/>
+            </>
         );
     }
 }
